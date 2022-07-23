0aeaa9289ad1d05eaed0e501efce8e0840b27ffd
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-07-23 14:08:03 +0530

    Pages Updated

--- scripts/programsfetch.js
+++ scripts/programsfetch.js
@@ -1,7 +1,8 @@
 let append =(data,cont) =>{
-    data.forEach(({img,category,title,subtitle}) => {
+    data.forEach(({logo,img,category,title,subtitle}) => {
         
         console.log(data)
+
         let image=document.createElement("img")
         image.src=img;
         image.style.height="50%";
@@ -26,9 +27,30 @@ let append =(data,cont) =>{
         sub.style.fontWeight="200";
         sub.style.fontSize="0.6rem";
         sub.style.paddingLeft="10px";
+        
 
         let div=document.createElement("div");
+        div.setAttribute("class","programs_cards");
         div.style.backgroundColor="white";
+
+
+
+        // let fb=document.createElement("div");
+        // fb.innerText="FB Plus";
+        // fb.style.position="absolute";
+        // fb.style.top="0";
+        // fb.style.right="0";
+        // fb.style.background="#3cdbb8";
+        // fb.style.color="#ffffff";
+        // fb.style.fontWeight="bold";
+        // fb.style.fontSize="1.2em";
+        // fb.style.textShadow="0px 0px 3px rgb(0 0 0 / 20%)";
+        // fb.style.padding="0.5rem 1rem";
+        // fb.style.borderRadius="0 0 0 6px";
+        // fb.style.borderBottom="2px solid #ffffff";
+        // fb.style.borderLeft="2px solid #ffffff";
+        // fb.style.zIndex="1";
+        // fb.style.cursor="pointer";
         // div.style.padding="10px"
 
         div.append(image,catg,tit,sub)
@@ -36,43 +58,4 @@ let append =(data,cont) =>{
     });
 }
 
-// let append2=(data,cont) =>{
-//     data.forEach(({img,category,title,subtitle}) => {
-        
-//         console.log(data)
-//         let image=document.createElement("img")
-//         image.src=img;
-//         image.style.height="50%";
-//         image.style.width="100%";
-
-//         let catg=document.createElement("p")
-//         catg.innerText=category;
-//         catg.style.color="#4296cb";
-//         catg.style.fontWeight="100";
-//         catg.style.fontSize="0.6rem";
-//         catg.style.paddingTop="7px";
-//         catg.style.paddingLeft="10px";
-
-//         let tit=document.createElement("h3")
-//         tit.innerText=title;
-//         tit.style.fontWeight="200";
-//         tit.style.fontSize="1rem";
-//         tit.style.paddingLeft="10px";
-
-//         let sub=document.createElement("p")
-//         sub.innerText=subtitle
-//         sub.style.fontWeight="200";
-//         sub.style.fontSize="0.6rem";
-//         sub.style.paddingLeft="10px";
-
-//         let div=document.createElement("div");
-//         div.style.backgroundColor="white";
-//         // div.style.padding="10px"
-
-//         div.append2(image,catg,tit,sub)
-//         cont.append2(div)
-//     });
-// }
-
-// export{append2}
 export{append}
